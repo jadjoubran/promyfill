b35b4536402fca951bbf579bc3b053dcfbc6fc2f
jadjoubran <joubran.jad@gmail.com>
2017-04-13 15:04:57 +0300

    Add fetch example

---
+++ examples/fetch/app.js
@@ -0,0 +1,24 @@
+/*promyfill*/
+!function(){function n(n){return new Promise(function(o,t){n&&o(n),t(n)})}function o(n){return new Promise(function(o,t){n||t();var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.defer=!0,e.src=n,e.onload=function(){o(n)},e.onerror=function(){t()},window.document.body.appendChild(e)})}window.promyfill=function(t,e){return new Promise(function(i){n(t).then(function(){i(t)}).catch(function(){o(e).then(function(){i(t)})})})}}();
+
+
+var usersList = document.getElementById('users-list');
+usersList.innerHTML += ' - JavaScript booted.';
+
+
+var fetchPolyfill = 'https://unpkg.com/unfetch@2.1.2/dist/unfetch.umd.js';
+promyfill('fetch' in window, fetchPolyfill).then(function(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(users) {
+        var template = "";
+        users.forEach(function(user) {
+            template += user.name + "\n";
+        });
+        usersList.innerHTML = template;
+    });
+});
+
+//other code that doesn't depend on fetch
